37ebf3c4cb9c56974c55feebba4f3c1bd299e74a
dorranh <dorran.howell@tweag.io>
2021-06-23 17:33:49 +0200

    Nest under let binding

--- src/example.ml
+++ src/example.ml
@@ -3,9 +3,13 @@ let [@inline] error_NegativeInput : int = 2
 
 
 let half_positive_integer (x: int) =
+  let foobar =
   if x < 0 then
     MyModuleWithFailures.failwith_error_code error_NegativeInput (* always considered uncovered, marked as red *)
   else if x = 0 then
     MyModuleWithFailures.failwith_error_code error_ZeroInput     (* always considered uncovered, marked as red *)
   else
     x / 2
+  in
+  let _ = Printf.printf "debugging" in
+  foobar
