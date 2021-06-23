8d1684d07a5c6295d1339b24e762b37b2125d39f
Georgios Karachalias <gdkaracha@gmail.com>
2021-06-23 17:46:49 +0200

    minimal example

--- src/example.ml
+++ src/example.ml
@@ -1,16 +1,15 @@
-let [@inline] error_ZeroInput     : int = 1
-let [@inline] error_NegativeInput : int = 2
+let error_ZeroInput     : int = 1
+let error_NegativeInput : int = 2
 
 let failwith_error_code (i: int) : 'a = Stdlib.failwith (string_of_int i)
 
 let half_positive_integer (x: int) =
   let foobar =
-  if x < 0 then
-    failwith_error_code error_NegativeInput (* always considered uncovered, marked as red *)
-  else if x = 0 then
-    failwith_error_code error_ZeroInput     (* always considered uncovered, marked as red *)
-  else
-    x / 2
+    if x < 0 then
+      failwith_error_code error_NegativeInput (* always considered uncovered, marked as red *)
+    else if x = 0 then
+      failwith_error_code error_ZeroInput     (* always considered uncovered, marked as red *)
+    else
+      x / 2
   in
-  let _ = Printf.printf "debugging" in
   foobar
